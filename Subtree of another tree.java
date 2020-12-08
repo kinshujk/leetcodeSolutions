8e8e8aed63cc3ac501c5f6846a51bdea4d5772a0
kinshujk <46257206+kinshujk@users.noreply.github.com>
2020-12-08 15:20:49 -0700

    Create Subtree of another tree.java

---
+++ Subtree of another tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return s != null && (sameTree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t));
+    }
+    public boolean sameTree(TreeNode s, TreeNode t)
+    {
+        if(s == null && t == null)
+            return true;
+        if(s == null || t == null)
+            return false;
+        return s.val == t.val && sameTree(s.left, t.left) && sameTree(s.right, t.right);
+    }
+}
